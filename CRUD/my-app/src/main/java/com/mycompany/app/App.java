77460173ba48f619bb1f5e154d3595f18c49df44
Christian Williams <williechri79@gmail.com>
2023-07-12 02:37:15 -0400

    Finished Pt 1: FileStorage CRUD has been successfully implimented with JAVA

--- CRUD/my-app/src/main/java/com/mycompany/app/App.java
+++ CRUD/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,40 +1,120 @@
 package com.mycompany.app;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class App {
-    public static void save(String key, String value, File file) {
-
+    public static void save(JSONObject object, File myFile) {
+        try {
+            PrintWriter writer = new PrintWriter(myFile, "UTF-8");
+            writer.println(object.toString());
+            writer.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println( "Saved :)" );
     }
-
-    public static void create(Scanner opScanner) {
+    
+    public static void create(JSONObject mainObj, Scanner opScanner, File myFile) {
         // POST
         System.out.println("What is your Key? ");
         String key = opScanner.nextLine();
+
+        if (mainObj.has(key)) {
+            System.out.println(key + " is in DB, can not create value that already exists");
+            return;
+        }
         System.out.println("What is your Value? ");
         String value = opScanner.nextLine();
 
-
-        System.out.println("POST" + key + value);
+        mainObj.put(key, value);
+        
+        // Stringify
+        System.out.println("POST: " + mainObj);
+        save(mainObj, myFile);
     }
 
-    public static void read() {
+    public static void readAll(JSONObject mainObj) {
         // GET
-        System.out.println("GET");
+        System.out.println("GET: " + mainObj);
     }
 
-    public static void update() {
+    public static void get(JSONObject mainObj, Scanner opScanner) {
+        // GET
+        System.out.println("What is your Key? ");
+        String key = opScanner.nextLine();
+        if (!mainObj.has(key)) {
+            System.out.println(key + " is not in DB, can not get value");
+            return;
+        }
+        System.out.println("GET: " + mainObj.get(key));
+    }
+
+    public static void update(JSONObject mainObj, Scanner opScanner, File myFile) {
         // PUT
-        System.out.println("UPDATE");
+        System.out.println("What is your Key? ");
+        String key = opScanner.nextLine();
+
+        if (!mainObj.has(key)) {
+            System.out.println(key + " is not in DB, can not update value");
+            return;
+        }
+
+        System.out.println("What is your Value? ");
+        String value = opScanner.nextLine();
+
+        System.out.println("UPDATE: " + key + ": " + mainObj.get(key) + " -> " + value );
+        mainObj.put(key, value);
+        System.out.println("UPDATE: " + key + ": " + mainObj.get(key) );
+
+        save(mainObj, myFile);
     }
 
-    public static void delete() {
+    public static void delete(JSONObject mainObj, Scanner opScanner, File myFile) {
         // DELETE
-        System.out.println("DELETE");
+        System.out.println("What is your Key? ");
+        String key = opScanner.nextLine();
+
+        System.out.println( "DELETE: " + key + ": " + mainObj.get(key) );
+        mainObj.remove(key);
+        System.out.println( "OBJECT: " + mainObj);
+        
+        save(mainObj, myFile);
+    } 
+
+    public static void clearAll(JSONObject mainObj, File myFile) {
+        // DELETE
+        Iterator<String> keys = mainObj.keys();
+        List<String> keyList = new ArrayList<String>();
+    
+        while (keys.hasNext()) {
+            keyList.add(keys.next());
+        }
+        System.out.println("Keylist: " + keyList);
+
+        while (keyList.size() > 0) {
+            mainObj.remove(keyList.get(0));
+            keyList.remove(0);
+        }
+
+        System.out.println( "All Clean! " + mainObj);
+  
+        save(mainObj, myFile);
     } 
 
     public static File createFile() {
@@ -54,23 +134,21 @@ public class App {
         return null;
     }
 
+    
+
     public static void main(String[] args) {
         Scanner opScan = new Scanner(System.in);
         String operation;
+        JSONObject mainObject = null;
 
         // Initalize file or database
-        createFile();
-
-
-        // Initialize input and output streams
-        // FileReader input = new FileReader(myFile);
-        // FileWriter output = new FileWriter(myFile);
+        File myFile = createFile();
 
         // get JSON object from file
         try {
             String lines = Files.readString(Paths.get("fileDB.txt"));
-            System.out.println("Read From file: " + lines);
-            
+            mainObject = new JSONObject(lines);   
+            System.out.println("loaded json object: " + mainObject);       
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -82,23 +160,41 @@ public class App {
             
             switch (operation) {
                 case "create":
-                    create(opScan);
+                    create(mainObject, opScan, myFile);
                     break;
-                case "read":
-                    // read(output);
+                case "readAll":
+                    readAll(mainObject);
                     break;
                 case "update":
-                    // update(input, output);
+                    update(mainObject, opScan, myFile);
+                    break;
+                case "get":
+                    get(mainObject, opScan);
                     break;
                 case "delete":
-                    // delete(input, output);
+                    delete(mainObject, opScan, myFile);
                     break;
+                case "clear":
+                    clearAll(mainObject, myFile);
+                    break;
+                case "quit":
+                    break;
+                case "man":
+                    System.out.println(" Operations: ");
+                    System.out.println(" create - push a key value to DB ");
+                    System.out.println(" readAll - get all keys and values from DB ");
+                    System.out.println(" get - get one value from a key ");
+                    System.out.println(" delete - delete one value from a key ");
+                    System.out.println(" clear - clears the whole DB ");
+                    System.out.println(" update - update a key value in the DB ");
+                    System.out.println(" quit - quit ");
+                    break;
+                default:
+                    System.out.println("Invalid Operation - Try again.");
             }
+            System.out.println("---------------------------------");
         } while (!operation.equalsIgnoreCase("quit"));
-
-        // save updated object to file
-        // input.close();
-        // output.close();
         opScan.close();
+        System.out.println("Good Night");
     }
 }
